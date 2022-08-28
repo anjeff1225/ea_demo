2ce7fec7d22cb4a41fb83713d0379777fc2caf98
anjeff1225 <jeff.sbian@gmail.com>
2022-08-28 16:02:38 -0700

    Put fake password for AWS account

--- test_cases/ui/test_upload_file.py
+++ test_cases/ui/test_upload_file.py
@@ -32,7 +32,7 @@ class TestUploadFile:
     @allure.title('Test Upload file to S3 Bucket success')
     def test_upload_file(self):
         # don't want to share my AWS account password, put a fake here
-        self.loginpage.login_method('jeff.sbian@gmail.com', 'Asdf123$%')
+        self.loginpage.login_method('jeff.sbian@gmail.com', 'fake_password')
         self.consolehomepage.click_bucket()
         self.bucketsmainpage.individual_bucket()
         self.individualbucketpage.upload_click()
